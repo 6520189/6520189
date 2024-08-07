57721c54281f878979d1dbba9ab648cf5390b7f5
Min La Wee Chan <u6520189@au.edu>
2024-08-07 09:42:41 +0700

    Initial commit for web-exam

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import accessoryData from "./accessory.json";
+import DataTable from "./components/DataTable";
+import Button from "react-bootstrap/Button";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [filteredSelectedItems, setFilteredSelectedItems] = useState([]);
+  const [price, setPrice] = useState(accessoryData[0].price);
+  const quantityRef = useRef();
+  const productRef = useRef();
+
+  const search = (keyword) => {
+    setFilteredSelectedItems(
+      selectedItems.filter(item => item.name.includes(keyword))
+    );
+  };
+
+  const handleSubmit = (e) => {
+    const productId = parseInt(productRef.current.value);
+    const product = accessoryData.find(accessory => accessory.id === productId);
+    const order = {
+      ...product,
+      quantity: quantityRef.current.value,
+    };
+    selectedItems.push(order);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  };
+
+  const updatePrice = (e) => {
+    const productId = parseInt(e.target.value);
+    const product = accessoryData.find(accessory => accessory.id === productId);
+    setPrice(product.price);
+  };
+
+  const deleteItemByIndex = (index) => {
+    selectedItems.splice(index, 1);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  };
+
+  const sortAscending = () => {
+    const sortedItems = [...selectedItems].sort((a, b) => a.name.localeCompare(b.name));
+    setFilteredSelectedItems(sortedItems);
+  };
+
+  const sortDescending = () => {
+    const sortedItems = [...selectedItems].sort((a, b) => b.name.localeCompare(a.name));
+    setFilteredSelectedItems(sortedItems);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Row>
+          <Col xs={2}>Product:</Col>
+          <Col xs={10}>
+            <select ref={productRef} onChange={updatePrice}>
+              {accessoryData.map((accessory, index) => (
+                <option key={index} value={accessory.id}>
+                  {accessory.name}
+                </option>
+              ))}
+            </select>
+          </Col>
+          <Col>Price:</Col>
+          <Col>{price}</Col>
+          <Col>Quantity:</Col>
+          <Col>
+            <input type="number" ref={quantityRef} defaultValue={1} />
+          </Col>
+        </Row>
+      </Container>
+      <Button onClick={handleSubmit}>Submit</Button>
+
+      <Container>
+        <Button variant="link" onClick={sortAscending}>
+          <i className="bi bi-arrow-up"></i>
+        </Button>
+        <Button variant="link" onClick={sortDescending}>
+          <i className="bi bi-arrow-down"></i>
+        </Button>
+        <span>Sort</span>
+        <DataTable 
+          data={filteredSelectedItems} 
+          onDelete={deleteItemByIndex}
+          onSearch={search}
+        />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
